3f402edfb1ac978a07f858c63198c82aa6ae00e5
afraniocosta <afraniompc@gmail>
2019-11-15 17:57:21 -0300

    criação do componente jobs

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './jobs.scss';
 import data from './jobs.json';
+import conta from './images/conta.png'
+
 
 function Jobs(){
 
+  const [showItems, setShowItems] = useState(false);  
+
   return(
     <>
-      <div className='container'>
-        <div className='row'> 
-          {data.map((item, index) => (
-            <div className="mb-5 col-lg-4">
-              <h2 className='mb-3'>{item.title}</h2>
-              <img className='img-fluid' src={item.img} alt="{item.title}"/>
-              <p className="text-right">
-                <a href="#">visitar</a>
-              </p>
-            </div>
-          ))}
+      <div className='jobs'>
+        <div className='container'>
+          <div className='row'> 
+                
+            {data.map((item, index) => (
+              
+              <div className='mb-5 col-lg-4'>
+
+                <img src={conta} className='img-fluid img-job' alt="Afrânio"/> 
+                
+                <p className='bg-more' onClick={() => setShowItems(!showItems)}>+</p>
+              
+                {showItems && 
+                  <div>
+                    
+                    <h2 className='mb-3'>{item.title}</h2>
+                    <p className='mb-3'>{item.description}</p>
+                    <p>Tecnologia: {item.framework}</p>
+                    
+                  </div>
+                }
+                
+                <p className='text-right text-link mt-3'>
+                  <a href={item.link} title={item.title}>visitar</a>
+                </p>
+
+              </div>
+
+              ))
+            }
+
+          </div>
         </div>
       </div>
     </>
