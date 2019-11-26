145a55bd33d2e658832e3c757126f1f53267a74d
afraniocosta <afraniompc@gmail>
2019-11-25 22:51:00 -0300

    alteração no layout jobs mobile

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -5,47 +5,82 @@ import data from './jobs.json';
 function Jobs(){
 
   const [showItems, setShowItems] = useState(false);
+  
+  const isMobile = window.innerWidth >= 768
 
   const imagemPortfolio = require.context('./images');
+
 
   return(
   
     <>
       <div className='jobs'>
         <div className='container'>
-          <div className='row'> 
+          <div className='wraper'> 
                 
             {data.map((item, index) => (
-              
-              <div className='mb-5 col-12 col-md-6 col-lg-4'>
 
-                <img src={imagemPortfolio(item.image)} className='img-fluid' />
-                
-                <p className='more' onClick={() => setShowItems(!showItems)} title='Ver o projeto'>{ showItems ? '-' : '+' }</p>
+            <div className='teste'>
+
+              <img src={imagemPortfolio(item.image)} className='img-fluid' />
+
+              <p className='more' onClick={() => setShowItems(!showItems)} title='Ver o projeto'>{ showItems ? '-' : '+' }</p>
+              {console.log(item)}
               
-                {showItems && 
-                  <div className='bg-content-job'>
-                    
-                    <h2 className='mb-3 title'>
-                      {item.title}
-                    </h2>
-                    
-                    <p className='mb-3 description'>
-                      {item.description}
-                    </p>
-                    
-                    <p className='tecnology'>
-                      Tecnologia: {item.framework}
-                    </p>
-                    
-                    <p className='text-link mt-5 text-link'>
-                      <a href={item.link} title={item.title}>visitar</a>
-                    </p>
-                    
-                  </div>
-                }
+              {showItems && 
+                <div className='bg-content-job'>
+                  
+                  <h2 className='mb-3 title'>
+                    {item.title}
+                  </h2>
+                  
+                  <p className='mb-3 description'>
+                    {item.description}
+                  </p>
+                  
+                  <p className='tecnology'>
+                    Tecnologia: {item.framework}
+                  </p>
+                  
+                  <p className='text-link mt-5 text-link'>
+                    <a href={item.link} title={item.title}>visitar</a>
+                  </p>
+                  
+                </div>
+              }
+
+            </div>
+              
+              // <div className='mb-5 col-12 col-md-6 col-lg-4'>
+
+              //   <img src={imagemPortfolio(item.image)} className='img-fluid' />
                 
-              </div>
+              //   <p className='more' onClick={() => setShowItems(!showItems)} title='Ver o projeto'>{ showItems ? '-' : '+' }</p>
+              //   {console.log(item)}
+              
+              //   {showItems && 
+              //     <div className='bg-content-job'>
+                    
+              //       <h2 className='mb-3 title'>
+              //         {item.title}
+              //       </h2>
+                    
+              //       <p className='mb-3 description'>
+              //         {item.description}
+              //       </p>
+                    
+              //       <p className='tecnology'>
+              //         Tecnologia: {item.framework}
+              //       </p>
+                    
+              //       <p className='text-link mt-5 text-link'>
+              //         <a href={item.link} title={item.title}>visitar</a>
+              //       </p>
+                    
+              //     </div>
+              //   }
+                
+              // </div>
 
               ))
             }
