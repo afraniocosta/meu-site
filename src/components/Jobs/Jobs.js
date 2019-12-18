90264a1fc53ce20db384154053925a6d3a4e3885
Afrânio Martins Pereira da Costa <bi001415@bhz-mt-001621.intermedium.local>
2019-12-18 17:41:51 -0300

    ajustes

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -4,19 +4,12 @@ import data from './jobs.json';
 
 function Jobs(){
 
-  // const [showItems, setShowItems] = useState(false);
-  // const [itens, updateItens] = useState([])
   const [itemAtivo, updateItemAtivo] = useState();
   
-  // const [currentItem, setCurrentItem] = useState(null)
-
-  // const isMobile = window.innerWidth >= 768
+  const isMobile = window.innerWidth <= 768
 
   const imagemPortfolio = require.context('./images');
 
-  // function handleCurrentItem (evt, newItem) {
-  //   setCurrentItem(newItem)
-  // }
 
   return(
   
@@ -30,7 +23,7 @@ function Jobs(){
             <div className='job mt-3'>
              
               { itemAtivo === index ?
-              
+                
                 <div className='bg-content-job'>
                   
                   <h2 className='mb-3 title'>
@@ -53,6 +46,7 @@ function Jobs(){
                   
                 </div>
                 :  
+                
                 <div>
                   <img src={imagemPortfolio(item.image)} className='img-fluid' alt={item.title}/>
                   <p className='more' onClick={() => updateItemAtivo(index)} title='Ver o projeto'>+</p>
@@ -60,37 +54,6 @@ function Jobs(){
               }
               
             </div>
-              
-              // <div className='mb-5 col-12 col-md-6 col-lg-4'>
-
-              //   <img src={imagemPortfolio(item.image)} className='img-fluid' />
-                
-              //   <p className='more' onClick={() => setShowItems(!showItems)} title='Ver o projeto'>{ showItems ? '-' : '+' }</p>
-              //   {console.log(item)}
-              
-              //   {showItems && 
-              //     <div className='bg-content-job'>
-                    
-              //       <h2 className='mb-3 title'>
-              //         {item.title}
-              //       </h2>
-                    
-              //       <p className='mb-3 description'>
-              //         {item.description}
-              //       </p>
-                    
-              //       <p className='tecnology'>
-              //         Tecnologia: {item.framework}
-              //       </p>
-                    
-              //       <p className='text-link mt-5 text-link'>
-              //         <a href={item.link} title={item.title}>visitar</a>
-              //       </p>
-                    
-              //     </div>
-              //   }
-                
-              // </div>
 
               ))
             }
