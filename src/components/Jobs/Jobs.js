5df36a1c0f5a600f458ea5d01df611f562f88570
Afrânio Martins Pereira da Costa <bi001415@bhz-mt-001621.intermedium.local>
2019-12-19 12:45:27 -0300

    ajuste cor da fonte mobile

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './jobs.scss';
 import data from './jobs.json';
 
@@ -12,51 +12,52 @@ function Jobs(){
 
 
   return(
-    
+  
     <>
       <div className='jobs'>
-        <div className='wraper'> 
-              
-          {data.map((item, index) => (
+        <div className='container'>
+          <div className='wraper'> 
+                
+            {data.map((item, index) => (
 
-          <div className='job mt-3'>
-            
-            { itemAtivo === index || isMobile ?
-              
-              <div className='bg-content-job'>
+            <div className='job mt-3'>
+             
+              { itemAtivo === index || isMobile ?
                 
-                <h2 className='mb-3 title'>
-                  {item.title}
-                </h2>
-                
-                <p className='mb-3 description'>
-                  {item.description}
-                </p>
-                
-                <p className='tecnology'>
-                  Tecnologia: {item.framework}
-                </p>
-                
-                <p className='text-link mt-5 text-link'>
-                  <a href={item.link} title={item.title}>visitar</a>
-                </p>
+                <div className='bg-content-job'>
+                  
+                  <h2 className='mb-3 title'>
+                    {item.title}
+                  </h2>
+                  
+                  <p className='mb-3 description'>
+                    {item.description}
+                  </p>
+                  
+                  <p className='tecnology'>
+                    Tecnologia: {item.framework}
+                  </p>
+                  
+                  <p className='text-link mt-5 text-link'>
+                    <a href={item.link} title={item.title}>visitar</a>
+                  </p>
 
-                <p className='less' onClick={() => updateItemAtivo(!index)} title='Fechar'>-</p>
-                
-              </div>
-              :  
+                  <p className='less' onClick={() => updateItemAtivo(!index)} title='Fechar'>-</p>
+                  
+                </div>
+                :  
+                <div>
+                  <img src={imagemPortfolio(item.image)} className='img-fluid' alt={item.title}/>
+                  <p className='more' onClick={() => updateItemAtivo(index)} title='Ver o projeto'>+</p>
+                </div>  
+              }
               
-              <div>
-                <img src={imagemPortfolio(item.image)} className='img-fluid' alt={item.title}/>
-                <p className='more' onClick={() => updateItemAtivo(index)} title='Ver o projeto'>+</p>
-              </div>  
+            </div>
+
+              ))
             }
             
           </div>
-
-            ))
-          }
-          
         </div>
       </div>
     </>
