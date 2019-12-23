a2ec8f9c405ae650751e1a34d48385c0e3609bc3
Afrânio Martins Pereira da Costa <bi001415@bhz-mt-001621.intermedium.local>
2019-12-23 15:13:58 -0300

    identaçao

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,41 +6,44 @@ import avatar from '../../assets/avatar.png'
 
 function Home () {
   return (
-      <>
-        <section>
-          <div className="container">
-            <div className="row">
-              <Header />
-            </div>
-          </div>  
-        </section>
-        
-        <div id="home" className="container mt-5">
-          
+    <>
+      <section>
+        <div className="container">
           <div className="row">
+            <Header />
+          </div>
+        </div>  
+      </section>
+      
+      <div id="home" className="container mt-5">
+        
+        <div className="row">
+        
+          <div className="col-sm-6 offset-1 col-md-6 col-lg-6 offset-lg-1 order-lg-2 mt-5">
+            <img src={avatar} className='circle-photo' alt="Afrânio"/>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-5 mt-3 ml-0">
           
-            <div className="col-sm-6 offset-1 col-md-6 col-lg-6 offset-lg-1 order-lg-2 mt-5">
-              <img src={avatar} className='circle-photo' alt="Afrânio"/>
-            </div>
+            <h2 className="mt-5 green-color">
+            Quem <span className='gray-color'>sou eu</span>
+            </h2>  
+          
+            <p className="mt-3 gray-text"> 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            </p>
 
-            <div className="col-12 col-md-6 col-lg-5 mt-3 ml-0">
-              <h2 className="mt-5 green-color">
-              Quem <span className='gray-color'>sou eu</span>
-              </h2>  
-            
-              <p className="mt-3 gray-text"> 
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              </p>
-
-              <p>saiba mais sobre mim</p>
-            
-            </div>
-
+            <p>saiba mais sobre mim</p>
+          
           </div>
 
         </div>
-      <Footer />
-      </>
+
+      </div>
+
+    <Footer />
+
+    </>
   )
 }
 
