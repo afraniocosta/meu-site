063b702efbe56646545a1a7e943185d056369d99
Afrânio Martins Pereira da Costa <bi001415@bhz-mt-001621.intermedium.local>
2019-12-23 13:07:27 -0300

    ajuste no nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,17 +5,35 @@ import './nav.scss'
 function Nav() {
 
 const [showNav, setShowNav] = useState(false); 
-// const isMobile = window.innerWidth <= 768
+const isMobile = window.innerWidth <= 375
+const isDesktop = window.innerWidth > 768
 
 
 return(
   <div className="navhome mr-3">
-    <button for="navicon" class="btn-hamburger" onClick={() => setShowNav(!showNav)}>
+    { isMobile ? <button for="navicon" class="btn-hamburger" onClick={() => setShowNav(!showNav)}>
       <div className='navOne'></div>
       <div className='navTwo'></div>
       <div className='navThree'></div>
-    </button>
-    { showNav && 
+    </button> : ''}
+    { isDesktop ? 
+    // criar um componente ul e chamar ele aqui em cada nav. Assim terei um apenas
+    <nav> 
+      <ul>
+        <li>
+          <Link to="/">página inicial</Link>
+        </li>
+        <li>  
+          <Link to="/sobre">sobre mim</Link>
+        </li>
+        <li>  
+          <Link to="/portfolio">portfolio</Link>
+        </li>
+        <li>  
+          <Link to="/contato">contato</Link>
+        </li>
+      </ul>
+    </nav>: showNav &&
     <nav> 
       <ul>
         <li>
